060a3386e136ed0fa4b76fd23519fe75cafc3d87
subedibiraj <subedi.biraj655@gmail.com>
2024-06-20 23:34:21 +0545

    connected to db with mongoose and created model for exercise and user

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+mongoose.connect(process.env.DB_URL)
+
+const UserSchema = new Schema({
+  username: String
+})
+
+const User = mongoose.model("User", UserSchema)
+
+const ExerciseSchema = new Schema({
+  username: {type: String, required: true},
+  description: String,
+  duration: Number,
+  date: Date
+})
+
+const Exercise = mongoose.model("Exercise",ExerciseSchema);
 
 app.use(cors())
 app.use(express.static('public'))
